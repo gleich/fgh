f3015fab2d6e7557a74f1be0cd5dcc0eacec9f8c
Matthew Gleich <email@mattglei.ch>
2020-10-22 23:23:44 -0400

    💚 Use username param for filter
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pkg/commands/remove/filter.go
+++ pkg/commands/remove/filter.go
@@ -1,7 +1,6 @@
 package remove
 
 import (
-	"github.com/Matt-Gleich/fgh/pkg/api"
 	"github.com/Matt-Gleich/fgh/pkg/commands/clone"
 	"github.com/Matt-Gleich/fgh/pkg/location"
 	"github.com/Matt-Gleich/statuser/v2"
@@ -9,7 +8,7 @@ import (
 
 // Filter out repos that don't have the owner and name passed in via args
 func FilterRepos(username string, repos []location.LocalRepo, args []string) (filtered []location.LocalRepo) {
-	owner, name := clone.OwnerAndName(api.Username(), args)
+	owner, name := clone.OwnerAndName(username, args)
 	for _, repo := range repos {
 		if (repo.Owner == owner) && (repo.Name == name) {
 			filtered = append(filtered, repo)
