01bd73643b08b6af1c59ec8aadb30c5e7fb8e845
Matthew Gleich <email@mattglei.ch>
2020-10-26 01:29:55 -0400

    ✨ Add skip flags to clean
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pkg/commands/clean.go
+++ pkg/commands/clean.go
@@ -16,14 +16,21 @@ var cleanCmd = &cobra.Command{
 	Short:                 fmt.Sprintf("%v Ask to remove old or deleted cloned repos", emoji.Soap),
 	Long:                  longDocStart + "https://github.com/Matt-Gleich/fgh#-fgh-clean",
 	Run: func(cmd *cobra.Command, args []string) {
-		flags := clean.ParseFlags(cmd)
-		clonedRepos := repos.Repos()
+		var (
+			flags       = clean.ParseFlags(cmd)
+			clonedRepos = repos.Repos()
+			toRemove    = []repos.LocalRepo{}
+		)
 
-		outdated := clean.GetOutdated(clonedRepos, flags.Years, flags.Months, flags.Days)
-		toRemove := clean.AskToRemoveOutdated(outdated)
+		if flags.SkipOutdated {
+			outdated := clean.GetOutdated(clonedRepos, flags.Years, flags.Months, flags.Days)
+			toRemove = append(toRemove, clean.AskToRemoveOutdated(outdated)...)
+		}
 
-		deleted := clean.GetDeleted(clonedRepos)
-		toRemove = append(toRemove, clean.AskToRemoveDeleted(deleted)...)
+		if flags.SkipDeleted {
+			deleted := clean.GetDeleted(clonedRepos)
+			toRemove = append(toRemove, clean.AskToRemoveDeleted(deleted)...)
+		}
 
 		clean.Remove(toRemove)
 		clean.CleanUp()
@@ -35,4 +42,6 @@ func init() {
 	cleanCmd.Flags().IntP("years", "y", 0, "Counts as outdated repo if it isn't modified locally in given number of years (default 0)")
 	cleanCmd.Flags().IntP("months", "m", 2, "Counts as outdated repo if it isn't modified locally in given number of months")
 	cleanCmd.Flags().IntP("days", "d", 0, "Counts as outdated repo if it isn't modified locally in given number of days (default 0)")
+	cleanCmd.Flags().Bool("skipOutdated", true, "Don't check for outdated repos")
+	cleanCmd.Flags().Bool("skipDeleted", true, "Don't check for deleted repos")
 }
--- pkg/commands/clean/flags.go
+++ pkg/commands/clean/flags.go
@@ -1,36 +1,51 @@
 package clean
 
 import (
+	"fmt"
+
 	"github.com/Matt-Gleich/statuser/v2"
 	"github.com/spf13/cobra"
 )
 
 type Flags struct {
-	Years  int
-	Months int
-	Days   int
+	Years        int
+	Months       int
+	Days         int
+	SkipOutdated bool
+	SkipDeleted  bool
 }
 
 // Parse arguments
 func ParseFlags(cmd *cobra.Command) Flags {
-	y, err := cmd.Flags().GetInt("years")
-	if err != nil {
-		statuser.Error("Failed to get years flag", err, 1)
-	}
-
-	m, err := cmd.Flags().GetInt("months")
-	if err != nil {
-		statuser.Error("Failed to get months flag", err, 1)
-	}
-
-	d, err := cmd.Flags().GetInt("days")
-	if err != nil {
-		statuser.Error("Failed to get days flag", err, 1)
-	}
-
 	return Flags{
-		Years:  y,
-		Months: m,
-		Days:   d,
+		Years:        getInt("years", cmd),
+		Months:       getInt("months", cmd),
+		Days:         getInt("days", cmd),
+		SkipOutdated: getBool("skipOutdated", cmd),
+		SkipDeleted:  getBool("skipDeleted", cmd),
+	}
+}
+
+// Get an flag with type int
+func getInt(name string, cmd *cobra.Command) int {
+	v, err := cmd.Flags().GetInt(name)
+	checkErr(err, name)
+	return v
+}
+
+// Get a flag with type bool
+func getBool(name string, cmd *cobra.Command) bool {
+	v, err := cmd.Flags().GetBool(name)
+	checkErr(err, name)
+	return v
+}
+
+// Check for an error when getting a flag
+func checkErr(err error, name string) {
+	if err != nil {
+		statuser.Error(
+			fmt.Sprintf("Failed to get %v flag", name),
+			err, 1,
+		)
 	}
 }
