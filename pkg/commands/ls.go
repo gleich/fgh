11b517aecbdd54823541c42535bf108358a3bee3
Matthew Gleich <email@mattglei.ch>
2020-10-30 13:29:59 -0400

    ✨ Add ls cmd
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

---
+++ pkg/commands/ls.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/Matt-Gleich/fgh/pkg/api"
+	"github.com/Matt-Gleich/fgh/pkg/repos"
+	"github.com/enescakir/emoji"
+	"github.com/spf13/cobra"
+)
+
+var lsCmd = &cobra.Command{
+	DisableFlagsInUseLine: true,
+	Use:                   "ls <OWNER/NAME>",
+	Short:                 fmt.Sprintf("%v  Get the path for cloned repo", emoji.Compass),
+	Args:                  cobra.ExactArgs(1),
+	Long:                  longDocStart + "https://github.com/Matt-Gleich/fgh#-fgh-remove",
+	Run: func(cmd *cobra.Command, args []string) {
+		filtered := repos.FilterRepos(api.Username(), repos.Repos(), args)
+		fmt.Println(filtered[0].Path)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lsCmd)
+}
