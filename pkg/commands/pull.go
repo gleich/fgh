e00eac666a7722284de854fe30879ef274c0ac54
Matthew Gleich <email@mattglei.ch>
2020-11-29 23:57:23 -0500

    ✨ Add pull cmd
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

---
+++ pkg/commands/pull.go
@@ -0,0 +1,28 @@
+package commands
+
+import (
+	"github.com/Matt-Gleich/fgh/pkg/commands/pull"
+	"github.com/Matt-Gleich/fgh/pkg/configuration"
+	"github.com/spf13/cobra"
+)
+
+var pullCmd = &cobra.Command{
+	DisableFlagsInUseLine: true,
+	Use:                   "pull",
+	Short:                 "Pull all repos that don't have any not pushed changes",
+	Args:                  cobra.NoArgs,
+	Long:                  longDocStart + "FILL THIS IN",
+	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			secrets     = configuration.GetSecrets()
+			config      = configuration.GetConfig()
+			clonedRepos = reposBasedOffCustomPath(cmd, config)
+		)
+		pull.PullRepos(secrets, clonedRepos)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pullCmd)
+	addCustomPathFlag(pullCmd)
+}
---
+++ pkg/commands/pull/pull.go
@@ -0,0 +1,69 @@
+package pull
+
+import (
+	"fmt"
+
+	"github.com/Matt-Gleich/fgh/pkg/commands/configure"
+	"github.com/Matt-Gleich/fgh/pkg/repos"
+	"github.com/Matt-Gleich/statuser/v2"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+)
+
+// Pull the latest changes for all local repos not in
+// a working state
+func PullRepos(secrets configure.SecretsOutline, clonedRepos []repos.LocalRepo) {
+	var pulled int
+	for _, repo := range clonedRepos {
+		// Outputting the status message
+		var (
+			statusMsg         string
+			pullChanges       bool
+			committed, pushed = repos.WorkingState(repo.Path)
+		)
+		if !committed && !pushed {
+			statusMsg = "changes not committed and not pushed"
+		} else if !committed {
+			statusMsg = "changes not committed"
+		} else if !pushed {
+			statusMsg = "changes not pushed"
+		} else {
+			pullChanges = true
+		}
+		if !pullChanges {
+			statuser.Warning(fmt.Sprintf("%v/%v has %v\n", repo.Owner, repo.Name, statusMsg))
+			continue
+		}
+
+		// Pulling latest changes
+		gitRepo, err := git.PlainOpen(repo.Path)
+		if err != nil {
+			statuser.Error("Failed to open repo in: "+repo.Path, err, 1)
+		}
+		workingTree, err := gitRepo.Worktree()
+		if err != nil {
+			statuser.Error("Failed to get working tree for "+repo.Path, err, 1)
+		}
+
+		err = workingTree.Pull(&git.PullOptions{
+			RemoteName: "origin",
+			Auth: &http.BasicAuth{
+				Username: secrets.Username,
+				Password: secrets.PAT,
+			},
+		})
+
+		// Outputting final message
+		if err != nil && err.Error() == "already up-to-date" {
+			fmt.Printf("%v/%v is already up to date\n", repo.Owner, repo.Name)
+			continue
+		}
+		if err != nil {
+			statuser.Error("Failed to pull changes for "+repo.Path, err, 1)
+		}
+		statuser.Success(fmt.Sprintf("Pulled latest changes for %v/%v", repo.Owner, repo.Name))
+		pulled++
+	}
+	fmt.Println()
+	statuser.Success(fmt.Sprintf("Pulled latest changes for %v repos", pulled))
+}
