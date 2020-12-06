4adecabc74f5ccc4e425a1ed453c9e291471958a
Matthew Gleich <email@mattglei.ch>
2020-12-05 20:11:39 -0500

    🔖 Update version tag

--- pkg/commands/root.go
+++ pkg/commands/root.go
@@ -36,7 +36,7 @@ Authors:
 	Run: func(cmd *cobra.Command, args []string) {
 		versionFlag := utils.GetBool("version", cmd)
 		if versionFlag {
-			version := "v2.2.1"
+			version := "v2.3.1"
 
 			spin := spinner.New(utils.SpinnerCharSet, utils.SpinnerSpeed)
 			spin.Suffix = " Checking for update"
