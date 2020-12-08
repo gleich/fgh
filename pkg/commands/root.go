78610fa3ec20f0f6ab87bc9c162cfbc0372967b6
Matthew Gleich <email@mattglei.ch>
2020-12-08 17:02:58 -0500

    🔖 Prep for v2.4.1

--- pkg/commands/root.go
+++ pkg/commands/root.go
@@ -36,7 +36,7 @@ Authors:
 	Run: func(cmd *cobra.Command, args []string) {
 		versionFlag := utils.GetBool("version", cmd)
 		if versionFlag {
-			version := "v2.4.0"
+			version := "v2.4.1"
 
 			spin := spinner.New(utils.SpinnerCharSet, utils.SpinnerSpeed)
 			spin.Suffix = " Checking for update"
