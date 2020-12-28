f7b52beab3bb31e5a12c5a2bf14bdc58d776ee1b
Matthew Gleich <email@mattglei.ch>
2020-12-27 19:38:28 -0500

    ♻️ Refactor clone to use git clone

--- pkg/commands/clone/git.go
+++ pkg/commands/clone/git.go
@@ -3,20 +3,18 @@ package clone
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/Matt-Gleich/fgh/pkg/api"
 	"github.com/Matt-Gleich/fgh/pkg/commands/configure"
-	"github.com/Matt-Gleich/fgh/pkg/utils"
 	"github.com/Matt-Gleich/statuser/v2"
 	"github.com/atotto/clipboard"
-	"github.com/briandowns/spinner"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 // Clone the repo
 func Clone(config configure.RegularOutline, secrets configure.SecretsOutline, repo api.Repo, path string) {
-	rawClone(secrets, repo, path)
+	rawClone(repo, path)
 	statuser.Success(fmt.Sprintf("Cloned %v/%v to:\n\t%v\n", repo.Owner, repo.Name, path))
 	if config.CloneClipboard {
 		err := clipboard.WriteAll(path)
@@ -27,25 +25,34 @@ func Clone(config configure.RegularOutline, secrets configure.SecretsOutline, re
 }
 
 // Raw function for cloning the repo
-func rawClone(secrets configure.SecretsOutline, repo api.Repo, path string) {
+func rawClone(repo api.Repo, path string) {
 	err := os.MkdirAll(path, 0777)
 	if err != nil {
 		statuser.Error("Failed to create folder at "+path, err, 1)
 	}
 
-	spin := spinner.New(utils.SpinnerCharSet, utils.SpinnerSpeed)
-	spin.Suffix = fmt.Sprintf(" Cloning %v/%v", repo.Owner, repo.Name)
-	spin.Start()
+	gitExecPath, err := exec.LookPath("git")
+	if err != nil {
+		statuser.Error("Failed to locate the git executable. Please install it or put in your PATH", err, 1)
+	}
 
-	_, err = git.PlainClone(path, false, &git.CloneOptions{
-		URL: fmt.Sprintf("https://github.com/%v/%v.git", repo.Owner, repo.Name),
-		Auth: &http.BasicAuth{
-			Username: secrets.Username,
-			Password: secrets.PAT,
+	err = os.Chdir(filepath.Dir(path))
+	if err != nil {
+		statuser.Error("Failed to change directory to the parent folder for "+path, err, 1)
+	}
+
+	cmd := &exec.Cmd{
+		Path: gitExecPath,
+		Args: []string{
+			gitExecPath,
+			"clone",
+			fmt.Sprintf("https://github.com/%v/%v.git", repo.Owner, repo.Name),
 		},
-	})
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
 
-	spin.Stop()
 	if err != nil {
 		statuser.Error("Failed to clone repo", err, 1)
 	}
