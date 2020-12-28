0ae57e6d7584d61e6b45f4f4fc7e6db694c34334
Matthew Gleich <email@mattglei.ch>
2020-12-27 19:41:56 -0500

    🔖 Prep for v2.5.5

--- pkg/commands/root.go
+++ pkg/commands/root.go
@@ -32,7 +32,7 @@ Repository: https://github.com/Matt-Gleich/fgh`,
 	Run: func(cmd *cobra.Command, args []string) {
 		versionFlag := utils.GetBool("version", cmd)
 		if versionFlag {
-			version := "v2.5.4"
+			version := "v2.5.5"
 
 			spin := spinner.New(utils.SpinnerCharSet, utils.SpinnerSpeed)
 			spin.Suffix = " Checking for update"
