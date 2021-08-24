83a85afa26180216ca0d59750f2e040e23d9a7b1
Caleb Denio <cjdenio44@gmail.com>
2021-08-24 13:07:01 -0400

    Support cloning over SSH

--- pkg/commands/clone/git.go
+++ pkg/commands/clone/git.go
@@ -6,11 +6,11 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/atotto/clipboard"
 	"github.com/gleich/fgh/pkg/api"
 	"github.com/gleich/fgh/pkg/commands/configure"
 	"github.com/gleich/fgh/pkg/utils"
 	"github.com/gleich/statuser/v2"
-	"github.com/atotto/clipboard"
 )
 
 // Clone the repo
@@ -20,7 +20,18 @@ func Clone(
 	repo api.Repo,
 	path string,
 ) utils.CtxErr {
-	rawClone(repo, path)
+	err := utils.CtxErr{}
+
+	if config.SSH {
+		err = sshClone(repo, path)
+	} else {
+		err = rawClone(repo, path)
+	}
+
+	if err.Error != nil {
+		return err
+	}
+
 	statuser.Success(fmt.Sprintf("Cloned %v/%v to:\n\t%v\n", repo.Owner, repo.Name, path))
 	if config.CloneClipboard {
 		err := clipboard.WriteAll(path)
@@ -66,6 +77,54 @@ func rawClone(repo api.Repo, path string) utils.CtxErr {
 			gitExecPath,
 			"clone",
 			fmt.Sprintf("https://github.com/%v/%v.git", repo.Owner, repo.Name),
+		},
+		Stdout: os.Stdout,
+		Stderr: os.Stdout,
+	}
+	err = cmd.Run()
+
+	if err != nil {
+		return utils.CtxErr{
+			Context: "Failed to clone repo",
+			Error:   err,
+		}
+	}
+
+	return utils.CtxErr{}
+}
+
+// Raw function for cloning the repo
+func sshClone(repo api.Repo, path string) utils.CtxErr {
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		return utils.CtxErr{
+			Context: "Failed to create folder at " + path,
+			Error:   err,
+		}
+	}
+
+	gitExecPath, err := exec.LookPath("git")
+	if err != nil {
+		return utils.CtxErr{
+			Context: "Failed to locate the git executable. Please install it or put in your PATH",
+			Error:   err,
+		}
+	}
+
+	err = os.Chdir(filepath.Dir(path))
+	if err != nil {
+		return utils.CtxErr{
+			Context: "Failed to change directory to the parent folder for " + path,
+			Error:   err,
+		}
+	}
+
+	cmd := &exec.Cmd{
+		Path: gitExecPath,
+		Args: []string{
+			gitExecPath,
+			"clone",
+			fmt.Sprintf("git@github.com:%v/%v.git", repo.Owner, repo.Name),
 		},
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
